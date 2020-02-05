f4d98c579f87797e235ae052ec90d90aca6a8722
Modular Magician <magic-modules@google.com>
2020-02-05 01:02:24 +0000

    New generated code for MM PR 3072.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ secret_version_basic/main.tf
@@ -0,0 +1,22 @@
+resource "google_secret_manager_secret" "secret-basic" {
+  provider = google-beta
+
+  secret_id = "secret-version-${local.name_suffix}"
+  
+  labels = {
+    label = "my-label"
+  }
+
+  replication {
+    automatic = true
+  }
+}
+
+
+resource "google_secret_manager_secret_version" "secret-version-basic" {
+  provider = google-beta
+
+  secret = google_secret_manager_secret.secret-basic.name
+
+  secret_data = "secret-data-${local.name_suffix}"
+}
