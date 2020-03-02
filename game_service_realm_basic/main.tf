091b048f671e038f73f23f481d1cd7e8d8b49223
Modular Magician <magic-modules@google.com>
2020-03-02 23:05:14 +0000

    New generated code for MM PR 3155.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ game_service_realm_basic/main.tf
@@ -0,0 +1,9 @@
+resource "google_game_services_realm" "default" {
+  provider = google-beta
+
+  realm_id  = "tf-test-realm-${local.name_suffix}"
+  time_zone = "EST"
+  location  = "global"
+
+  description = "one of the nine"
+}
