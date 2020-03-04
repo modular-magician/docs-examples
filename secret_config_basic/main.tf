bfb88145e722d1b6b2f185d9169e24119c472295
Modular Magician <magic-modules@google.com>
2020-03-04 08:44:54 +0000

    Old generated code for MM PR 3201.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ secret_config_basic/main.tf
@@ -0,0 +1,20 @@
+resource "google_secret_manager_secret" "secret-basic" {
+  provider = google-beta
+
+  secret_id = "secret-${local.name_suffix}"
+  
+  labels = {
+    label = "my-label"
+  }
+
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+      replicas {
+        location = "us-east1"
+      }
+    }
+  }
+}
