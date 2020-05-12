e28d80f796e5b7710ceb1dae2595179e1b40ae29
Modular Magician <magic-modules@google.com>
2020-05-12 00:15:28 +0000

    Old generated code for MM PR 3491.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_connection_basic/main.tf
@@ -0,0 +1,26 @@
+resource "google_sql_database_instance" "instance" {
+    provider         = google-beta
+    name             = "my-database-instance-${local.name_suffix}"
+    database_version = "POSTGRES_11"
+    region           = "us-central1"
+    settings {
+		tier = "db-f1-micro"
+	}
+}
+
+resource "google_sql_database" "db" {
+    provider = google-beta
+    instance = google_sql_database_instance.instance.name
+    name     = "db"
+}
+
+resource "google_bigquery_connection" "connection" {
+    provider      = google-beta
+    friendly_name = "👋"
+    description   = "a riveting description"
+    cloud_sql {
+        instance_id = google_sql_database_instance.instance.connection_name
+        database    = google_sql_database.db.name
+        type        = "POSTGRES"
+    }
+}
