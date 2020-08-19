af9dae97faaf486c526c426ac6371d4ea19b7430
Modular Magician <magic-modules@google.com>
2020-08-19 15:19:21 +0000

    Old generated code for MM PR 3885.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ notebook_instance_basic/main.tf
@@ -0,0 +1,10 @@
+resource "google_notebooks_instance" "instance" {
+  provider = google-beta
+  name = "notebooks-instance-${local.name_suffix}"
+  location = "us-west1-a"
+  machine_type = "n1-standard-1"
+  vm_image {
+    project      = "deeplearning-platform-release"
+    image_family = "tf-latest-cpu"
+  }
+}
