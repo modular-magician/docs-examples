0e3bb8de525c3dc4f25b3ec94dd6ff3995752d34
Modular Magician <magic-modules@google.com>
2020-08-28 07:21:21 +0000

    New generated code for MM PR 3927.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ address_with_shared_loadbalancer_vip/main.tf
@@ -0,0 +1,6 @@
+resource "google_compute_address" "internal_with_shared_loadbalancer_vip" {
+  provider     = google-beta 
+  name         = "my-internal-address-${local.name_suffix}"
+  address_type = "INTERNAL"
+  purpose      = "SHARED_LOADBALANCER_VIP"
+}
