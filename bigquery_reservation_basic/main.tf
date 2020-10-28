45b084ecdc59b858e78c40753cd434d455dfe6f2
Modular Magician <magic-modules@google.com>
2020-10-28 23:26:53 +0000

    New generated code for MM PR 4161.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ bigquery_reservation_basic/main.tf
@@ -0,0 +1,9 @@
+resource "google_bigquery_reservation" "reservation" {
+	provider       = google-beta
+	name           = "reservation-${local.name_suffix}"
+	location       = "asia-northeast1"
+	// Set to 0 for testing purposes
+	// In reality this would be larger than zero
+	slot_capacity  = 0
+	ignore_idle_slots = false
+}
