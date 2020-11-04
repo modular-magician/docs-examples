b594d5bc3431b2f19da7d030451f692c6e35d30e
Modular Magician <magic-modules@google.com>
2020-11-04 17:09:51 +0000

    Old generated code for MM PR 4190.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ apigateway_gateway_basic/main.tf
@@ -0,0 +1,23 @@
+resource "google_api_gateway_api" "api_gw" {
+  provider = google-beta
+  api_id = "api-gw-${local.name_suffix}"
+}
+
+resource "google_api_gateway_api_config" "api_gw" {
+  provider = google-beta
+  api = google_api_gateway_api.api_gw.api_id
+  api_config_id = "api-gw-${local.name_suffix}"
+
+  openapi_documents {
+    document {
+      path = "spec.yaml"
+      contents = filebase64("test-fixtures/apigateway/openapi.yaml")
+    }
+  }
+}
+
+resource "google_api_gateway_gateway" "api_gw" {
+  provider = google-beta
+  api_config = google_api_gateway_api_config.api_gw.id
+  gateway_id = "api-gw-${local.name_suffix}"
+}
