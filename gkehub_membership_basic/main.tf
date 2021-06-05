c64d367a4da7d43db9d8493d3332dae0791c3222
Modular Magician <magic-modules@google.com>
2021-06-05 18:40:01 +0000

    Old generated code for MM PR 4847.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ gkehub_membership_basic/main.tf
@@ -0,0 +1,17 @@
+resource "google_container_cluster" "primary" {
+  name               = "basiccluster-${local.name_suffix}"
+  location           = "us-central1-a"
+  initial_node_count = 1
+  provider = google-beta
+}
+
+resource "google_gke_hub_membership" "membership" {
+  membership_id = "basic-${local.name_suffix}"
+  endpoint {
+    gke_cluster {
+      resource_link = "//container.googleapis.com/${google_container_cluster.primary.id}"
+    }
+  }
+  description = "test resource."
+  provider = google-beta
+}
