0f6601dd8784ab2ca8a13011834d5ced17d8bd55
Modular Magician <magic-modules@google.com>
2021-06-06 18:13:15 +0000

    New generated code for MM PR 4848.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- backend_service_network_endpoint/main.tf
+++ backend_service_network_endpoint/main.tf
@@ -1,16 +1,19 @@
 resource "google_compute_global_network_endpoint_group" "external_proxy" {
+  provider=google-beta
   name                  = "network-endpoint-${local.name_suffix}"
   network_endpoint_type = "INTERNET_FQDN_PORT"
   default_port          = "443"
 }
 
 resource "google_compute_global_network_endpoint" "proxy" {
+  provider=google-beta
   global_network_endpoint_group = google_compute_global_network_endpoint_group.external_proxy.id
   fqdn                          = "test.example.com"
   port                          = google_compute_global_network_endpoint_group.external_proxy.default_port
 }
 
 resource "google_compute_backend_service" "default" {
+  provider=google-beta
   name                            = "backend-service-${local.name_suffix}"
   enable_cdn                      = true
   timeout_sec                     = 10
