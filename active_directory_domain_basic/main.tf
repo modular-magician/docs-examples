879cd029c051165df725ad5eb2e45a8ab735cf80
Modular Magician <magic-modules@google.com>
2023-01-14 07:29:47 +0000

    New generated code for MM PR 6717.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- active_directory_domain_basic/main.tf
+++ active_directory_domain_basic/main.tf
@@ -1,5 +1,10 @@
 resource "google_active_directory_domain" "ad-domain" {
-  domain_name       = "tfgen-${local.name_suffix}.org.com"
-  locations         = ["us-central1"]
-  reserved_ip_range = "192.168.255.0/24" 
+  domain_name         = "tfgen-${local.name_suffix}.org.com"
+  locations           = ["us-central1"]
+  reserved_ip_range   = "192.168.255.0/24"
+  authorized_networks = [google_compute_network.default.id]
+}
+
+resource "google_compute_network" "default" {
+  name = "tfgen-${local.name_suffix}"
 }
