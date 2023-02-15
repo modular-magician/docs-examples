bedb8b895e082ab28bf11e3a8154aca4fa5bb3df
Modular Magician <magic-modules@google.com>
2023-02-15 20:01:37 +0000

    New generated code for MM PR 7297.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- tpu_node_basic/main.tf
+++ tpu_node_basic/main.tf
@@ -4,9 +4,9 @@ data "google_tpu_tensorflow_versions" "available" {
 
 resource "google_tpu_node" "tpu" {
   name = "test-tpu-${local.name_suffix}"
-  zone = "us-central1-b"
+  zone = "us-central1-c"
 
-  accelerator_type   = "v3-8"
+  accelerator_type   = "v2-8"
   tensorflow_version = data.google_tpu_tensorflow_versions.available.versions[0]
   cidr_block         = "10.2.0.0/29"
 }
