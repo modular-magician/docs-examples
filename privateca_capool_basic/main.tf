40269c856d7a720f887fdcc6d628b7712431c068
Modular Magician <magic-modules@google.com>
2023-08-02 05:49:57 +0000

    New generated code for MM PR 8496.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- privateca_capool_basic/main.tf
+++ privateca_capool_basic/main.tf
@@ -2,11 +2,4 @@ resource "google_privateca_ca_pool" "default" {
   name = "my-pool-${local.name_suffix}"
   location = "us-central1"
   tier = "ENTERPRISE"
-  publishing_options {
-    publish_ca_cert = true
-    publish_crl = true
-  }
-  labels = {
-    foo = "bar"
-  }
 }
