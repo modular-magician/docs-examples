0b8e917280316b90f78a8bfb54a61b91a4b78eef
Modular Magician <magic-modules@google.com>
2023-08-08 04:29:14 +0000

    New generated code for MM PR 8512.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ edgecontainer_cluster_with_maintenance_window/main.tf
@@ -0,0 +1,40 @@
+resource "google_edgecontainer_cluster" "default" {
+  name = "cluster-with-maintenance-${local.name_suffix}"
+  location   = "us-central1"
+
+  authorization {
+    admin_users {
+      username = "admin@hashicorptest.com"
+    }
+  }
+
+  networking {
+    cluster_ipv4_cidr_blocks = ["10.0.0.0/16"]
+    services_ipv4_cidr_blocks = ["10.1.0.0/16"]
+  }
+
+  fleet {
+    project = "projects/${data.google_project.project.number}"
+  }
+
+  maintenance_policy {
+    window {
+      recurring_window {
+        window {
+          start_time = "2023-01-01T08:00:00Z"
+          end_time = "2023-01-01T17:00:00Z"
+        }
+        
+        recurrence = "FREQ=WEEKLY;BYDAY=SA"
+      }
+    }
+  }
+
+  labels = {
+    test = "cluster-with-maintenance-${local.name_suffix}"
+  }
+
+  default_max_pods_per_node = 128
+}
+
+data "google_project" "project" {}
