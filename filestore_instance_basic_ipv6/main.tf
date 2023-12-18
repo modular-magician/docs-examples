adc14d42b7306dcc7a5355d0bb8e7ed64e1994fe
Modular Magician <magic-modules@google.com>
2023-12-18 17:22:35 +0000

    New generated code for MM PR 9309.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ filestore_instance_basic_ipv6/main.tf
@@ -0,0 +1,15 @@
+resource "google_filestore_instance" "instance" {
+  name     = "test-instance-${local.name_suffix}"
+  location = "us-central1-b"
+  tier     = "BASIC_HDD"
+
+  file_shares {
+    capacity_gb = 1024
+    name        = "share1"
+  }
+
+  networks {
+    network = "default"
+    modes   = ["MODE_IPV6"]
+  }
+}
