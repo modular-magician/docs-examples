046f3294d225d1bfebb5a87ca0b35769bf7bab2d
Modular Magician <magic-modules@google.com>
2024-09-26 06:42:56 +0000

    New generated code for MM PR 11816.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_connectivity_group_basic/main.tf
@@ -0,0 +1,18 @@
+resource "google_network_connectivity_hub" "basic_hub"  {
+ name        = "hub1-${local.name_suffix}"
+ description = "A sample hub"
+ labels = {
+    label-one = "value-one"
+  }
+}
+
+resource "google_network_connectivity_group" "primary"  {
+ hub         = google_network_connectivity_hub.basic_hub.id
+ name        = "default"
+ auto_accept {
+    auto_accept_projects = [
+      "foo-${local.name_suffix}", 
+      "bar-${local.name_suffix}", 
+    ]
+  }
+}
