4e5c37cfb905852982105b81093d9b36d7f502bb
Modular Magician <magic-modules@google.com>
2024-12-06 00:58:54 +0000

    New generated code for MM PR 12507.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ region_backend_service_internal_self_managed_lb_scheme/main.tf
@@ -0,0 +1,16 @@
+resource "google_compute_region_backend_service" "default" {
+  name                            = "region-service-${local.name_suffix}"
+  region                          = "us-central1"
+  health_checks                   = [google_compute_region_health_check.health_check.id]
+
+  load_balancing_scheme = "INTERNAL_SELF_MANAGED"
+}
+
+resource "google_compute_region_health_check" "health_check" {
+  name               = "rbs-health-check-${local.name_suffix}"
+  region             = "us-central1"
+
+  tcp_health_check {
+    port = 80
+  }
+}
