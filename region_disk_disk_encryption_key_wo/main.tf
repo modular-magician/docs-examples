1c0d13ce91366353c303afa5ee77a188a4ef178a
Modular Magician <magic-modules@google.com>
2025-02-13 07:50:53 +0000

    New generated code for MM PR 12789.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ region_disk_disk_encryption_key_wo/main.tf
@@ -0,0 +1,26 @@
+resource "google_compute_region_disk" "regiondisk" {
+  name                      = "my-region-disk-${local.name_suffix}"
+  snapshot                  = google_compute_snapshot.snapdisk.id
+  type                      = "pd-ssd"
+  region                    = "us-central1"
+  physical_block_size_bytes = 4096
+
+  replica_zones = ["us-central1-a", "us-central1-f"]
+}
+
+resource "google_compute_disk" "disk" {
+  name  = "my-disk-${local.name_suffix}"
+  image = "debian-cloud/debian-11"
+  size  = 50
+  type  = "pd-ssd"
+  zone  = "us-central1-a"
+  disk_encryption_key = {
+    raw_key_wo = "write_only_key-${local.name_suffix}"
+  }
+}
+
+resource "google_compute_snapshot" "snapdisk" {
+  name        = "my-snapshot-${local.name_suffix}"
+  source_disk = google_compute_disk.disk.name
+  zone        = "us-central1-a"
+}
