a04727363c539740742ed402c1c4738bd5d8e6b1
Modular Magician <magic-modules@google.com>
2025-07-10 18:16:14 +0000

    New generated code for MM PR 14498.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- dns_managed_zone_cloud_logging/main.tf
+++ dns_managed_zone_cloud_logging/main.tf
@@ -1,6 +1,6 @@
 resource "google_dns_managed_zone" "cloud-logging-enabled-zone" {
   name        = "cloud-logging-enabled-zone-${local.name_suffix}"
-  dns_name    = "services.example.com."
+  dns_name    = "fmt.Sprintf("services.example.com-%s.", context["random_suffix"])-${local.name_suffix}"
   description = "Example cloud logging enabled DNS zone"
   labels = {
     foo = "bar"
