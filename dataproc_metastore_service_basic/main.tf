da9325054e7bc8a3f8a07403c4d3d4601154e54c
Modular Magician <magic-modules@google.com>
2025-08-20 17:49:47 +0000

    New generated code for MM PR 14893.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- dataproc_metastore_service_basic/main.tf
+++ dataproc_metastore_service_basic/main.tf
@@ -3,6 +3,7 @@ resource "google_dataproc_metastore_service" "default" {
   location   = "us-central1"
   port       = 9080
   tier       = "DEVELOPER"
+  deletion_protection = false
 
   maintenance_window {
     hour_of_day = 2
