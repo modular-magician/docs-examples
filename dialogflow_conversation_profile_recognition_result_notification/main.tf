52cf9659ecf0055c0b3e5b2cbd0068a3c78feba6
Modular Magician <magic-modules@google.com>
2025-09-17 23:31:05 +0000

    New generated code for MM PR 15129.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ dialogflow_conversation_profile_recognition_result_notification/main.tf
@@ -0,0 +1,26 @@
+resource "google_dialogflow_agent" "basic_agent" {
+  display_name = "example_agent"
+  default_language_code = "en-us"
+  time_zone = "America/New_York"
+}
+resource "google_dialogflow_conversation_profile" "basic_profile" {
+  display_name = "dialogflow-profile-${local.name_suffix}"
+  location = "global"
+  automated_agent_config {
+    agent = "projects/${google_dialogflow_agent.basic_agent.id}/locations/global/agent/environments/draft"
+  }
+  human_agent_assistant_config {
+    message_analysis_config {
+      enable_entity_extraction  = true
+      enable_sentiment_analysis = true
+    }
+  }
+  new_recognition_result_notification_config {
+    topic = google_pubsub_topic.basic_profile.id
+    message_format = "JSON"
+  }
+}
+
+resource "google_pubsub_topic" "basic_profile" {
+  name = "dialogflow-profile-${local.name_suffix}"
+}
