a979892938725da43c9b777d01a5cc433781ca09
Modular Magician <magic-modules@google.com>
2025-12-04 05:08:53 +0000

    New generated code for MM PR 15877.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ compute_interconnect_attachment_tags/main.tf
@@ -0,0 +1,24 @@
+resource "google_compute_interconnect_attachment" "interconnect-attachment-with-tags" {
+  name                     = "interconnect-attachment-with-tags-${local.name_suffix}"
+  edge_availability_domain = "AVAILABILITY_DOMAIN_1"
+  type                     = "PARTNER"
+  router                   = google_compute_router.foobar.id
+  mtu                      = 1500
+  labels                   = { mykey = "myvalue" }
+  resource_manager_tags = {
+    "tagKeys/123" = "tagValues/456"
+  }
+}
+
+resource "google_compute_router" "foobar" {
+  name    = "router-1-${local.name_suffix}"
+  network = google_compute_network.foobar.name
+  bgp {
+    asn = 16550
+  }
+}
+
+resource "google_compute_network" "foobar" {
+  name                    = "network-1-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
