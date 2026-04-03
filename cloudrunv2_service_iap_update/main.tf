9dd0ea8c56ff2b9e3f09531aa7ff0d4e0515e7fd
Modular Magician <magic-modules@google.com>
2026-04-03 21:04:14 +0000

    New generated code for MM PR 16932.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ cloudrunv2_service_iap_update/main.tf
@@ -0,0 +1,15 @@
+resource "google_cloud_run_v2_service" "default" {
+  name     = "cloudrun-iap-service-${local.name_suffix}"
+  location = "us-central1"
+  deletion_protection = false
+  ingress = "INGRESS_TRAFFIC_ALL"
+  iap_enabled = false
+  scaling {
+    max_instance_count = 100
+  }
+  template {
+    containers {
+      image = "us-docker.pkg.dev/cloudrun/container/hello"
+    }
+  }
+}
