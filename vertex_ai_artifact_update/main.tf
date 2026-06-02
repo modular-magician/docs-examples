ed87ecb712e5fc572a17558b0eeeb533696d16e5
Modular Magician <magic-modules@google.com>
2026-06-02 16:45:03 +0000

    New generated code for MM PR 17635.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ vertex_ai_artifact_update/main.tf
@@ -0,0 +1,26 @@
+resource "google_vertex_ai_metadata_store" "store" {
+  name        = "store-${local.name_suffix}"
+  description = "Store to test Artifact"
+  region      = "us-central1"
+}
+
+resource "google_vertex_ai_artifact" "artifact" {
+  artifact_id    = "artifact-${local.name_suffix}"
+  metadatastore  = google_vertex_ai_metadata_store.store.name
+  location       = "us-central1"
+  schema_title   = "system.Dataset"
+  schema_version = "0.0.1"
+  uri            = "https://example.com/dataset"
+  state          = "LIVE"
+
+  description  = "An updated artifact description"
+  display_name = "Updated Display Name"
+  
+  labels = {
+    label = "value"
+  }
+
+  metadata = {
+    key = "value"
+  }
+}
