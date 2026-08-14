fe4a5c55f2e148d359ced2fe4f563466f1ad78c8
Modular Magician <magic-modules@google.com>
2026-08-14 13:50:13 +0000

    New generated code for MM PR 18652.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- oracledatabase_cloud_exadata_infrastructure_exascale_config_basic/main.tf
+++ oracledatabase_cloud_exadata_infrastructure_exascale_config_basic/main.tf
@@ -18,4 +18,6 @@ resource "google_oracle_database_cloud_exadata_infrastructure_exascale_config" "
   location                     = "us-east4"
   project                      = "my-project-${local.name_suffix}"
   total_storage_size_gb        = 10240
+  total_vm_storage_size_gb     = 2048
+
 }
