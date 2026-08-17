f07a3f39d30157514e8e834d29db2c0314cfc698
Modular Magician <magic-modules@google.com>
2026-08-17 17:50:52 +0000

    New generated code for MM PR 18666.
    Signed-off-by: Modular Magician <magic-modules@google.com>

---
+++ network_connectivity_spoke_accept_update/main.tf
@@ -0,0 +1,21 @@
+resource "google_compute_network" "network" {
+  name                    = "net-${local.name_suffix}"
+  auto_create_subnetworks = false
+}
+
+resource "google_network_connectivity_hub" "basic_hub" {
+  name        = "hub1-${local.name_suffix}"
+  description = "A sample hub"
+}
+
+resource "google_network_connectivity_spoke" "primary" {
+  name        = "spoke-accept-${local.name_suffix}"
+  location    = "global"
+  description = "Updated spoke"
+  hub         = google_network_connectivity_hub.basic_hub.id
+  auto_accept_hub = true
+
+  linked_vpc_network {
+    uri = google_compute_network.network.self_link
+  }
+}
