7728d1c37222381499892f5b7eb4a819b1dd8739
Modular Magician <magic-modules@google.com>
2026-08-24 20:18:28 +0000

    New generated code for MM PR 13553.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- ssl_certificate_basic_writeonly/main.tf
+++ ssl_certificate_basic_writeonly/main.tf
@@ -1,9 +1,10 @@
 resource "google_compute_ssl_certificate" "default" {
-  name_prefix            = "my-certificate-"
-  description            = "a description"
-  private_key_wo         = file("../static/ssl_cert/test.key")
-  private_key_wo_version = parseint(filesha256("../static/ssl_cert/test.key"),16)%pow(2,32)
-  certificate            = file("../static/ssl_cert/test.crt")
+  name_prefix              = "my-certificate-"
+  description              = "a description"
+  private_key_wo           = file("../static/ssl_cert/test.key")
+  private_key_wo_version   = parseint(filesha256("../static/ssl_cert/test.key"),16)%pow(2,32)
+  certificate_wo           = file("../static/ssl_cert/test.crt")
+  certificate_wo_version   = parseint(filesha256("../static/ssl_cert/test.crt"),16)%pow(2,32)
 
   lifecycle {
     create_before_destroy = true
