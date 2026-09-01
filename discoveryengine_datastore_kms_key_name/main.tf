8e7408e9824982dd2c7115d8d39bb3bd13ca0377
Modular Magician <magic-modules@google.com>
2026-09-01 17:07:53 +0000

    New generated code for MM PR 18829.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- discoveryengine_datastore_kms_key_name/main.tf
+++ discoveryengine_datastore_kms_key_name/main.tf
@@ -8,4 +8,23 @@ resource "google_discovery_engine_data_store" "kms_key_name" {
   kms_key_name                 = "kms-key-${local.name_suffix}"
   create_advanced_site_search  = false
   skip_default_schema_creation = false
+
+  depends_on = [google_discovery_engine_cmek_config.default]
+}
+
+resource "google_discovery_engine_cmek_config" "default" {
+  location       = "us"
+  cmek_config_id = "cmek-config-id-${local.name_suffix}"
+  kms_key        = "kms-key-${local.name_suffix}"
+  set_default    = false
+
+  depends_on = [google_kms_crypto_key_iam_member.crypto_key]
+}
+
+data "google_project" "project" {}
+
+resource "google_kms_crypto_key_iam_member" "crypto_key" {
+  crypto_key_id = "kms-key-${local.name_suffix}"
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:service-${data.google_project.project.number}@gcp-sa-discoveryengine.iam.gserviceaccount.com"
 }
