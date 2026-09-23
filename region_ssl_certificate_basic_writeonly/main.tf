113c52f52576aca6a75bc137d1d2ca2736927860
Modular Magician <magic-modules@google.com>
2026-09-23 17:26:49 +0000

    New generated code for MM PR 13553.
    Signed-off-by: Modular Magician <magic-modules@google.com>

--- region_ssl_certificate_basic_writeonly/main.tf
+++ region_ssl_certificate_basic_writeonly/main.tf
@@ -1,12 +1,9 @@
 resource "google_compute_region_ssl_certificate" "default" {
   region                 = "us-central1"
-  name_prefix            = "my-certificate-"
+  name                   = "my-certificate-${local.name_suffix}"
   description            = "a description"
   private_key_wo         = file("../static/ssl_cert/test.key")
   private_key_wo_version = parseint(filesha256("../static/ssl_cert/test.key"),16)%pow(2,32)
-  certificate            = file("../static/ssl_cert/test.crt")
-
-  lifecycle {
-    create_before_destroy = true
-  }
+  certificate_wo         = file("../static/ssl_cert/test.crt")
+  certificate_wo_version = parseint(filesha256("../static/ssl_cert/test.crt"),16)%pow(2,32)
 }
